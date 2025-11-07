c718cda71d95ff09ec360a339301c309d46023f3
MdRaiyanTashfiquee <raiyantashfiquee01@gmail.com>
2025-11-07 16:06:43 +0600

    23-54591-3

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string repeat;
+
+            do
+            {
+                Console.WriteLine("=== Simple Calculator ===");
+                Console.WriteLine("Select an operation:");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.Write("Your choice (1-4): ");
+                string option = Console.ReadLine();
+
+                Console.Write("Enter first number: ");
+                double first = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                double second = double.Parse(Console.ReadLine());
+
+                double answer;
+
+                switch (option)
+                {
+                    case "1":
+                        answer = first + second;
+                        Console.WriteLine($"Result = {answer}");
+                        break;
+
+                    case "2":
+                        answer = first - second;
+                        Console.WriteLine($"Result = {answer}");
+                        break;
+
+                    case "3":
+                        answer = first * second;
+                        Console.WriteLine($"Result = {answer}");
+                        break;
+
+                    case "4":
+                        if (second == 0)
+                            Console.WriteLine("Error: Division by zero!");
+                        else
+                            Console.WriteLine($"Result = {first / second}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection. Try again!");
+                        break;
+                }
+
+                Console.Write("\nDo you want another calculation? (yes/no): ");
+                repeat = Console.ReadLine().Trim().ToLower();
+                Console.WriteLine();
+
+            } while (repeat == "yes");
+
+            Console.WriteLine("Program ended. Goodbye!");
+        }
+    }
+}
---
+++ Mortgage Calculator/Mortgage Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Mortgage Calculator ");
+
+        Console.Write("Enter loan amount: ");
+        double P = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter annual interest rate (in %): ");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter loan period (in years): ");
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        double monthlyInterestRate = (r / 100) / 12;
+        int n = t * 12;
+
+        double m =
+            (P * r * Math.Pow(1 + r, n)) / (Math.Pow(1 + r, n) - 1);
+
+        double totalPaid = m * n;
+        double totalInterest = totalPaid - P;
+        CultureInfo bdCulture = new CultureInfo("bn-BD");
+
+        Console.WriteLine($"Monthly Payment: {m.ToString("C", bdCulture)}");
+        Console.WriteLine($"Total Paid: {totalPaid:0,0.00}৳");
+        Console.WriteLine($"Total Interest: {totalInterest:0,0.00}৳");
+    }
+}
+
+
+
+
